2367c85e42a45291447bd174081e3fe8330b089d
ahmed <wezonaser50@gmail.com>
2023-09-11 13:08:16 +0300

    fixing dist folder

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <StoreProvider>
-            <BrowserRouter>
+            <BrowserRouter basename='/summrize-app-tsx/'>
                 <App />
             </BrowserRouter>
         </StoreProvider>
