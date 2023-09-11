6e162b361d5d9cb04ac5270631c969155fe4beb1
ahmed <wezonaser50@gmail.com>
2023-09-11 07:27:07 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/summrize-app-tsx/',
-  plugins: [react()],
+    base: '/summrize-app-tsx/',
+    plugins: [react()],
 })
